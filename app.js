743e4d71fd7e44a16e93cf78352e58cc2abf8b54
Murod-beck <murodbeck.off@gmail.com>
2023-07-09 20:05:37 +0300

    game

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-const cards = document.querySelector(".card");
-const count1 = document.querySelector(".count-1");
-const count2 = document.querySelector(".count-2");
-const current1 = document.querySelector(".current-1");
-const current2 = document.querySelector(".current-2");
 const filter = document.querySelector(".filters");
 const newgame = document.querySelector(".new-game");
 const hold = document.querySelector(".hold");
@@ -20,8 +15,56 @@ const values = [
   "filter_9",
 ];
 
+let currents = 0;
+let activePlayer = 0;
+let holds = [0, 0];
+
+const switchPlayer = () => {
+  currents = 0;
+  document.querySelector(`.current-${activePlayer}`).textContent = currents;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  document.querySelector(".card-0").classList.toggle("blue-grey");
+  document.querySelector(".card-1").classList.toggle("blue-grey");
+};
+
+const gameOver = () => {
+  hold.classList.add("disabled");
+  roll.classList.add("disabled");
+  document.querySelector(`.game-over${activePlayer}`).style.display = "block";
+};
+
 roll.addEventListener("click", () => {
   const randomFilter = Math.trunc(Math.random() * values.length);
   filter.textContent = values[randomFilter];
-  current1.textContent = randomFilter + 1;
+  if (randomFilter !== 0) {
+    currents += randomFilter + 1;
+    document.querySelector(`.current-${activePlayer}`).textContent = currents;
+  } else {
+    switchPlayer();
+  }
+});
+
+hold.addEventListener("click", () => {
+  holds[activePlayer] += currents;
+  document.querySelector(`.count-${activePlayer}`).textContent =
+    holds[activePlayer];
+  if (holds[activePlayer] >= 100) {
+    gameOver();
+  } else switchPlayer();
+});
+
+newgame.addEventListener("click", () => {
+  hold.classList.remove("disabled");
+  roll.classList.remove("disabled");
+  document.querySelector(`.game-over${activePlayer}`).style.display = "none";
+  currents = 0;
+  activePlayer = 0;
+  holds = [0, 0];
+  filter.textContent = "filter";
+  document.querySelector(".count-0").textContent = 0;
+  document.querySelector(".count-1").textContent = 0;
+  document.querySelector(".current-0").textContent = 0;
+  document.querySelector(".current-1").textContent = 0;
+  document.querySelector(".card-0").classList.add("blue-grey");
+  document.querySelector(".card-1").classList.remove("blue-grey");
 });
